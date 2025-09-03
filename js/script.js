22cf2281ee78cd9884571aa9de4b9f0d60dd98b3
João Victor Mendes <joaodev21@gmail.com>
2025-09-03 11:12:31 -0300

    feat(nav): Adiciona responsividade e interatividade ao menu mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+// =================== SELEÇÃO DOS ELEMENTOS DO HTML ===================
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
+
+// =================== LÓGICA DO MENU MOBILE ===================
+// Evento para MOSTRAR o menu lateral
+menuBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'block';
+});
+
+// Evento para FECHAR o menu lateral
+closeBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'none';
+});
+
+// =================== LÓGICA DO TEMA CLARO/ESCURO ===================
+ //Adiciona um "escutador" de eventos de clique ao botão
+ themeToggler.addEventListener('click', () => {
+  //Adiciona ou remove a classe do tema escuro body
+  document.body.classList.toggle('dark-theme-variables');
+
+  //Altera a classe 'active nos ícones de sol e lua 
+  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active'); 
+  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+ });
+
+
+
+// =================== PREENCHIMENTO DA TABELA DE PEDIDOS ===================
 //Pega os dados do array 'Orders' (do arquivo orders.js) e itera sobre cada pedido 
  Orders.forEach(order => {
   // Para cada pedido, cria um elemento de linha de tabeça (tr)
@@ -19,18 +50,10 @@ const trContent =`
   document.querySelector('table tbody').appendChild(tr);
  });
 
- // Seleciona o botão de tema no documento 
- const themeToggler = document.querySelector('.theme-toggler');
 
- //Adiciona um "escutador" de eventos de clique ao botão
- themeToggler.addEventListener('click', () => {
-  //Adiciona ou remove a classe do tema escuro body
-  document.body.classList.toggle('dark-theme-variables');
 
-  //Altera a classe 'active nos ícones de sol e lua 
-  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active'); 
-  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
- });
+
+
 
 
 
